14c7c06b8a6acb996a0cf88b71483ba3a2acb431
alexleybourne <alexleybourne1@gmail.com>
2021-04-07 23:29:37 +1000

    Now checks it can run on page and gets the input value

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-// document.body.style.backgroundColor = 'red';
-console.log('Loaded!:');
-const luckyButton = document.body.getElementsByClassName("RNmpXc")[1];
-console.log('Old Button:');
-console.log(luckyButton);
+// Checking if the page is a google search results page
+const canRun = !window.location.href.includes('search')
+console.log('CAN RUN:', canRun);
+console.log('LOCATION:', window.location.href)
 
 const stackSearch = () => {
     console.log("stackSearch has been summoned");
-    const inputVal = document.body.getElementsByClassName("gLFyf").value;
-    console.log('Input Value:', inputVal);
-    // window.location.href = `https://stackoverflow.com/search?q=${inputVal}`;
+    const searchValue = document.querySelector('[title="Search"]').value;
+    console.log('Input Value:', searchValue);
+    // window.location.href = `https://stackoverflow.com/search?q=${searchVal}`;
 }
 
 document.addEventListener('click', function (event) {
+    // Don't want it running on google search / result pages
+    if (!canRun) return;
 
-    if (!event.target.matches('.Stack-Overflow-Button')) return;
+    // if (!event.target.matches('.Stack-Overflow-Button')) return
 
 	// Don't follow the link
 	event.preventDefault();
@@ -24,12 +25,14 @@ document.addEventListener('click', function (event) {
 
 }, false);
 
+if (canRun) {
+    const luckyButton = document.getElementsByClassName('RNmpXc')[1]
+    const newButton = document.createElement("button");
+    newButton.innerHTML = "Stack Overflow";
+    newButton.classList.add('Stack-Overflow-Button');
+    newButton.onclick = (() => stackSearch());
+    console.log('New Button vvvvv');
+    console.log(newButton);
+    luckyButton.replaceWith(newButton);
+}
 
-const newButton = document.createElement("button");
-newButton.innerHTML = "Stack Overflow";
-newButton.classList.add('Stack-Overflow-Button');
-newButton.onclick = (() => stackSearch());
-console.log('New Button vvvvv');
-console.log(newButton);
-
-luckyButton.replaceWith(newButton);
