41a065c903c1c4542615ce96a483fa460bd3ff25
alexleybourne <alexleybourne1@gmail.com>
2021-04-09 07:21:35 +1000

    Can now use /s command and enter to search stack overflow

--- script.js
+++ script.js
@@ -1,28 +1,54 @@
 // Checking if the page is google search but not a google search results page
 const canRun = !window.location.href.includes('search') && window.location.href.includes('www.google.com');
 
-const stackSearch = () => {
+const getSearchValue = () => {
     const searchValue = document.querySelector('[title="Search"]').value;
     // If the value is not blank it continues
     if (searchValue.length) {
         // Replaces spaces in the query with pluses to work in url query
         const formattedValue = searchValue.replace(/\s/g, '+');
-        // Sends the new url to the search bar and visits it
-        window.location.href = `https://stackoverflow.com/search?q=${formattedValue}`;
+        // Sends the cleaned up value back
+        return formattedValue;
     }
 }
 
-document.addEventListener('click', function (event) {
+const stackSearch = () => {
+    // gets the input value from the function
+    const search = getSearchValue();
+    // checks it has returned a value
+    if (search) {
+        // visits the search
+        window.location.href = `https://stackoverflow.com/search?q=${search}`;
+    }
+}
+
+document.addEventListener('click', (e) => {
     // Don't want it running on google search / result pages
     if (!canRun) return;
     // If we have not clicked on the stack overflow button we want the page 
     // to behave the same as normal
-    if (!event.target.matches('.Stack-Overflow-Button')) return
+    if (!e.target.matches('.Stack-Overflow-Button')) return
 	// Prevent default click action that google does ( searches )
-	event.preventDefault();
+	e.preventDefault();
     // Now we run our search Function
     stackSearch();
 }, false);
+
+
+document.addEventListener('keydown', (e) => {
+    // Gets the search value and checks for the "/s" command
+    const search = getSearchValue();
+    const stackSearch = search.includes('/s+');
+    document.querySelector('[title="Search"]').value;
+    // Checking it can run and it is the enter key ( 13 = enter key )
+    if (e.code == "Enter" && canRun && stackSearch) {
+        e.preventDefault();
+        // removes "/s+" from the start of our query
+        const searchCleaned = search.slice(3);
+        console.log('STACK SEARCH!', searchCleaned);
+        window.location.href = `https://stackoverflow.com/search?q=${searchCleaned}`;
+    }
+});
 
 // This runs if this is the correct page
 if (canRun) {
